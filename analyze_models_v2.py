53f28c80ccd072985638f19cff0d0a82a3d9e20a
Christian Brown <christian@Christians-MacBook-Pro-3.local>
2018-03-17 11:25:56 -0700

    attempt to fix git push bug

--- analyze_models_v2.py
+++ analyze_models_v2.py
@@ -167,13 +167,17 @@ print(np.shape(labels_vals[0]))
 print(np.shape(sentence_vals))
 print(np.shape(sentence_vals[0]))
 
+pkl_output = output_vals[0]
+pkl_preds = prediction_vals[0]
+pkl_labels = labels_vals[0]
+pkl_sentences = sentence_vals[0]
 
 # write to cPickle
 # pickle.dump(output_vals, open( "output_vals_small.pkl", "wb" ) 
-pickle.dump(output_vals[0], open( "output_vals.pkl", "wb" ) )
-pickle.dump(prediction_vals[0], open( "prediction_vals.pkl", "wb" ) )
-pickle.dump(labels_vals[0], open( "labels_vals.pkl", "wb" ) )
-pickle.dump(sentence_vals[0], open( "sentence_vals.pkl", "wb" ) )
+pickle.dump(pkl_output, open( "output_vals.pkl", "wb" ) )
+pickle.dump(pkl_preds, open( "prediction_vals.pkl", "wb" ) )
+pickle.dump(pkl_labels, open( "labels_vals.pkl", "wb" ) )
+pickle.dump(pkl_sentences, open( "sentence_vals.pkl", "wb" ) )
 
 # TODO: Dump a few other useful things as well
 
