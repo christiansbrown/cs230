2e29c8d603e804d90d172af09eb455af4a7713c7
Christian Brown <christian@Christians-MacBook-Pro-3.local>
2018-03-20 00:59:55 -0700

    epoch break to pickle results for local debugging

--- analyze_models_v2_sent.py
+++ analyze_models_v2_sent.py
@@ -193,6 +193,9 @@ with tf.Session() as sess:
 		label_vals.append(step_labels)
 		sentence_vals.append(step_sentences)
 
+        if i > 5:
+            break        
+
 	# Extract values for metrics
 	metrics_values = {k: v[0] for k, v in eval_metrics.items()}
 	metrics_val = sess.run(metrics_values)
